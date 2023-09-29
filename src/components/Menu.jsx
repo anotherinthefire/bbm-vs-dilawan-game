18be5dd3649176297861b2faac235898c6470fb9
anotherinthefire <rongodfreyultra@gmail.com>
2023-09-30 06:50:21 +0800

    game menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,27 @@
+import SmallButton from "./microcomponents/SmallButton"
+import logo from "../assets/images/logo.svg"
+import { useContext } from "react"
+import { GameContext } from "./contexts/GameContext"
+
+export const Menu = () => {
+  const { setGameState, initialState, setConnected, setWinner, paused, setPaused, setSeconds } = useContext(GameContext)
+
+  const togglePause = () => {
+    setPaused(!paused)
+  }
+
+  const restart = () => {
+    setGameState(initialState);
+    setWinner(0);
+    setConnected([]);
+    setSeconds(30);
+  }
+
+  return (
+    <div className="flex justify-between items-center mt-10 mb-8 select-none max-w-[632px]">
+      <SmallButton text="MENU" do={togglePause} />
+      <img src={logo} alt="logo" />
+      <SmallButton text="RESTART" do={restart} />
+    </div>
+  )
+}
---
+++ src/components/microcomponents/SmallButton.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+const SmallButton = (props) => {
+  return(
+    <button className="h-[40px] text-center bg-darkerpurple text-white capitalize rounded-[20px] px-[20px] hover:bg-red duration-300" onClick={props.do}>{props.text}</button>
+  )
+}
+
+export default SmallButton
