f22cb1f2d668c989c40fd43a29a7c5fbdd91d67f
anotherinthefire <rongodfreyultra@gmail.com>
2023-09-30 06:45:33 +0800

    sets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      "black": '#000000',
+      "darkerpurple": '#5C2DD5',
+      'purple': '#7945FF',
+      "red": "#FD6687",
+      "yellow": "#FFCE67",
+      "white": "#FFFFFF",
+      "grey": "#262829",
+      "p": "#575757",
+    },
+    extend: {
+      boxShadow: {
+        shadd: '0 9px 0px 0px #000000',
+      }
+    },
   },
   plugins: [],
 }
-
