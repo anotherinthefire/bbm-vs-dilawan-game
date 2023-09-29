d1d7df6ca329d53370cdcccaeeb8d1e74f3fb5dc
anotherinthefire <rongodfreyultra@gmail.com>
2023-09-30 06:52:19 +0800

    game base

---
+++ src/pages/Game.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import '../App'
+import Board from "../components/Board";
+import { Menu } from '../components/Menu';
+import { GameContext } from "../components/contexts/GameContext";
+import { PlayerBarMobile } from '../components/PlayerBarMobile';
+import { BigBottomThing } from '../components/BigBottomThing';
+
+
+const Game = () => {
+  const [selectedColumn, setSelectedColumn] = useState(null);
+  const [connected, setConnected] = useState([])
+  const [wins, setWins] = useState([0, 0]);
+  const [winner, setWinner] = useState(0);
+  const [paused, setPaused] = useState(false);
+  const [seconds, setSeconds] = useState(30);
+  const [draw, setDraw] = useState(false);
+
+  const initialState = [
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+  ]
+
+  const [gameState, setGameState] = useState(initialState);
+
+  const [playerTurn, setPlayerTurn] = useState(1);
+
+  let isWonOrDraw = winner !== 0 || (gameState.slice(0, 7).every(element => element !== 0) && winner === 0)
+
+  const turnChange = () => {
+    if (playerTurn === 1) {
+      setPlayerTurn(2)
+    } else {
+      setPlayerTurn(1)
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center ">
+      <div>
+        <GameContext.Provider value={{ selectedColumn, setSelectedColumn, initialState, gameState, setGameState, playerTurn, turnChange, connected, setConnected, wins, setWins, winner, setWinner, paused, setPaused, seconds, setSeconds, draw, setDraw, isWonOrDraw }}>
+          <Menu />
+          <PlayerBarMobile />
+          <Board />
+          <BigBottomThing />
+        </GameContext.Provider>
+      </div>
+    </div>
+  );
+}
+
+export default Game
