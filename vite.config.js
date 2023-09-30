e0f84d7a1276a335189ca22fffc37296be4625be
anotherinthefire <rongodfreyultra@gmail.com>
2023-09-30 11:17:09 +0800

    build build

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-    manifest: {
-      name: 'Connect 4 Game',
-      short_name: 'Conn4',
-      description: 'BBM vs Dilawan || Player vs Playe',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: './src/assets/images/logo.svg',
-          sizes: '192x192',
-          type: 'image/svg'
-        },
-        {
-          src: './src/assets/images/logo.svg',
-          sizes: '512x512',
-          type: 'image/svg'
-        }
-      ]
-    }
-  })],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'BBM vs Dilawan',
+        short_name: 'Conn4',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/favicon-32x32.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-32x32.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
