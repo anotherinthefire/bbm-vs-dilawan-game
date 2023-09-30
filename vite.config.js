c69cf6dd6b38649c618b5747bc36eef3c2de6753
anotherinthefire <rongodfreyultra@gmail.com>
2023-09-30 11:08:43 +0800

    build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,24 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    injectRegister: 'auto'
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      name: 'Connect 4 Game',
+      short_name: 'Conn4',
+      description: 'BBM vs Dilawan || Player vs Playe',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: './src/assets/images/logo.svg',
+          sizes: '192x192',
+          type: 'image/svg'
+        },
+        {
+          src: './src/assets/images/logo.svg',
+          sizes: '512x512',
+          type: 'image/svg'
+        }
+      ]
+    }
   })],
 })
